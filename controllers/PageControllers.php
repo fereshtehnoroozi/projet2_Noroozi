25b43cfcf5c8ca7bb5bf04844dc856c4849f22c1
fereshteh noroozi <fereshtehn2008@yahoo.com>
2023-12-20 12:11:39 -0500

    Gestion page routes

---
+++ controllers/PageControllers.php
@@ -0,0 +1,126 @@
+<?php
+
+require_once './models/ProductModel.php';
+require_once './models/UserModel.php';
+require_once './models/CartModel.php';
+
+class PageController
+{
+    private $productModel;
+    private $userModel;
+    private $cartModel;
+
+    public function __construct()
+    {
+        $this->productModel = new ProductModel();
+        $this->userModel = new UserModel();
+        $this->cartModel = new CartModel();
+    }
+
+
+    public function home()
+    {
+        // Implement logic to display the home page
+        include 'views/home.php'; // Adjust the view file path
+    }
+
+    public function products()
+    {
+        // Implement logic to display a list of products
+        $products = $this->productModel->getAll('product');
+        include 'views/products.php'; // Adjust the view file path
+    }
+
+    public function viewProduct($productId)
+    {
+        // Implement logic to retrieve and display the details of a specific product
+        $product = $this->productModel->getProductById($productId);
+        include 'views/product.php'; // Adjust the view file path
+    }
+
+    public function cart()
+    {
+        // Implement logic to handle the shopping cart
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Check if it's an add to cart, update cart, or remove from cart operation
+            if (isset($_POST['add_to_cart'])) {
+                $productId = $_POST['product_id'];
+                $quantity = $_POST['quantity'];
+                $this->cartModel->addToCart($productId, $quantity);
+            } elseif (isset($_POST['update_cart'])) {
+                $cartData = $_POST['cart'];
+                $this->cartModel->updateCart($cartData);
+            } elseif (isset($_POST['remove_from_cart'])) {
+                $productId = $_POST['product_id'];
+                $this->cartModel->removeFromCart($productId);
+            }
+
+            header('Location: ./cart');
+            exit();
+        }
+
+        $cartItems = $this->cartModel->getCartItems();
+        include 'views/cart'; // Adjust the view file path
+    }
+
+    public function signup()
+    {
+        // Implement logic to handle user registration
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['register'])) {
+            $userData = [
+                'email' => $_POST['email'],
+                'username' => $_POST['username'],
+                'fname' => $_POST['fname'],
+                'lname' => $_POST['lname'],
+                'pwd' => $_POST['pwd'],
+            ];
+
+            $this->userModel->addUser($userData);
+            header('Location: ./login');
+            exit();
+        }
+
+        include 'views/signup.php'; // Adjust the view file path
+    }
+
+    public function login()
+    {
+        // Implement logic to handle user login
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
+            $this->userModel;
+            // Redirect or show a success message
+            header('Location: ./home');
+            exit();
+        }
+
+        include 'views/login.php'; // Adjust the view file path
+    }
+
+    public function logout()
+    {
+        //traitement de log out 
+
+
+
+        // Redirect or show a success message
+        header('Location: ./home');
+        exit();
+    }
+
+
+    public function users()
+    {
+
+        //get all my users 
+        $oUserModel = new UserModel;
+        $users = $oUserModel->getUsers();
+
+        $viewData = [
+            'users' => $users
+        ];
+        global $viewData;
+        // Redirect or show a success message
+        include 'views/manageUsers.php';
+        exit();
+    }
+}
---
+++ utils/routes.php
@@ -0,0 +1,109 @@
+<?php
+//session_start();
+require_once './controllers/PageControllers.php';
+$url = $_SERVER['REQUEST_URI'];
+
+
+$urltrim = ltrim($url, '/Ecommerc_2/project2_Noroozi');
+$explodeUrl = explode('/', $url);
+var_dump($explodeUrl[3]);
+switch ($explodeUrl[3]) {
+
+    case 'products':
+
+        $page = new PageController;
+        $page->products();
+
+        break;
+
+
+    case 'product':
+        // Récupérer l'ID du produit à partir de la requête GET
+        $productId = isset($explodeUrl[4]) ? $explodeUrl[4] : null;
+        if ($productId) {
+            $page = new PageController;
+            $page->products($productId);
+            // echo " je suis dans product";
+        } else {
+            echo "Product ID is missing.";
+        }
+
+        break;
+
+
+    case 'cart':
+        $page = new PageController;
+        $page->cart();
+        break;
+
+    case 'signup':
+        $page = new PageController;
+        $page->signup();
+        break;
+
+
+    case 'login':
+        $page = new PageController;
+        $page->login();
+        // ou PageController::login
+
+        break;
+
+    case 'users':
+        $page = new PageController;
+        $userId = isset($explodeUrl[4]) ? $explodeUrl[4] : null;
+        if (isset($_SESSION['auth']['role_id']) && ($_SESSION['auth']['role_id'] === 2 || $_SESSION['auth']['role_id'] === 1)) {
+
+            if ($userId) {
+                $page->deleteUserById($explodeUrl[4]);
+                echo " je suis dans user ";
+            } else {
+                $page->users();
+            }
+        }
+        break;
+
+    case 'user':
+
+        $userId = isset($explodeUrl[4]) ? $explodeUrl[4] : null;
+        if ($userId) {
+            $page = new PageController;
+            $page->users($userId);
+            echo " je suis dans users ";
+        } else {
+            echo "UserID is missing.";
+        }
+
+        $page = new PageController;
+        $page->users();
+
+
+        break;
+
+
+
+
+    case 'profile':
+        // Récupérer l'ID du user à partir de la requête GET
+        $id = isset($explodeUrl[4]) ? $explodeUrl[4] : null;
+        if ($id) {
+            $page = new PageController;
+            $page->profile($id);
+            //echo " je suis dans profile";
+        } else {
+            echo "Profile ID is missing.";
+        }
+
+        break;
+
+        // $page = new PageController;
+        // $page->profile();
+
+        // break;
+
+
+    default:
+        $page = new PageController;
+        $page->home();
+        break;
+}
